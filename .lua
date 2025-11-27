a935d4f3fa57ada271257c7c3b4b2ff9267970c3
TheeNov <111340353+TheeNov@users.noreply.github.com>
2025-11-26 22:45:18 -0600

    Update .lua

--- .lua
+++ .lua
@@ -5,4 +5,7 @@ if Whitelist then
   Whitelist["HoneyWheresmypant5"] = 3189400807
   Whitelist["Misiniall"] = 8386805282
   Whitelist["misinial"] = 1
+  Whitelist["vellishing"] = 4931713374
+  Whitelist["Iuxeys"] = 887943597
+  Whitelist["zxshaarps"] = 2968704744
 end
