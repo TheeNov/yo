12f47a6a152a14545edc5159a0cb3a22aa904688
TheeNov <111340353+TheeNov@users.noreply.github.com>
2025-12-09 20:27:01 -0600

    Update .lua

--- .lua
+++ .lua
@@ -10,7 +10,7 @@ if Whitelist then
   Whitelist["Damgoem"] = 286983362
   Whitelist["xornum"] = 1461498491
   Whitelist["ubu7"] = 72117821
-  --Misiniall
+  --Lux
   Whitelist["Robloxianb1r9j0f5"] = 8386805282
   Whitelist["BoardwalkDefender9"] = 59858049
   Whitelist["Ka7ic"] = 1778013108
