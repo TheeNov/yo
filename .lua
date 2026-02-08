7e4fde488528f9ef3220fd7a171cb65a2fb29630
TheeNov <111340353+TheeNov@users.noreply.github.com>
2026-02-08 04:05:02 -0600

    Update .lua

--- .lua
+++ .lua
@@ -1,3 +1,39 @@
-warn("This script is down for maintenance.")
 Honorable = true
-if 3 == 3 then return end
+
+if Whitelist then
+  Whitelist["CrownedHealer"] = 7858117098
+  --Lux
+  Whitelist["vellishing"] = 4931713374
+  Whitelist["evilcatboy127"] = 3055198568
+  Whitelist["trappedbymymemories"] = 887943597
+  --Valk
+  Whitelist["Damgoem"] = 286983362
+  Whitelist["xornum"] = 1461498491
+  Whitelist["ubu7"] = 72117821
+  --Misinial
+  Whitelist["Robloxianb1r9j0f5"] = 8386805282
+  Whitelist["BoardwalkDefender9"] = 59858049
+  Whitelist["Ka7ic"] = 1778013108
+  --Deva
+  Whitelist["vmrced"] = 629093680
+  Whitelist["xvlxrxc"] = 1625210793
+  Whitelist["eq4x"] = 1392525916
+  --Acy
+  Whitelist["InloveWithMyGothGF"] = 155907036
+  --Whitelist["roblox_user_3045569379"] = 3045569379
+  Whitelist["PureAspirations"] = 38310538
+  --Zycular
+  Whitelist["1984Drip"] = 2707463079
+  Whitelist["HoneyWheresmypant5"] = 3189400807
+  Whitelist["TotallyTPWLK"] = 28513912
+  --Duran
+  Whitelist["halorecoil"] = 4228647541
+  Whitelist["waksy"] = 19773260
+  --vvvvv
+  Whitelist["9265z"] = 4750402284
+  --Dane
+  Whitelist["DaneCenat"] = 19773260
+  Whitelist["danesimp2"] = 417881038
+  Whitelist["halorecoil"] = 4228647541
+  
+end
